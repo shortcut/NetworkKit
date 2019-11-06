7c973a877ad952f94009f9906f11fa14067d25e9
Andre Navarro <andre.navarro@gmail.com>
2019-11-06 10:20:31 +0100

    add an example project

---
+++ Examples/NetworkKitDemo/NetworkKitDemo/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  NetworkKitDemo
+//
+//  Created by Andre Navarro on 11/4/19.
+//  Copyright © 2019 Shortcut AS. All rights reserved.
+//
+
+import UIKit
+import NetworkKit
+
+class Cell: UICollectionViewCell {
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.addSubview(self.imageView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = self.contentView.bounds
+    }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+        self.imageView.cancelImageLoad()
+    }
+}
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    var network: Network = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = ["Accept": "application/json"]
+
+        
+        let network = Network(urlSessionConfiguration: configuration)
+
+        return network
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.register(Cell.self, forCellWithReuseIdentifier: "Cell")
+        
+        let request = network.request("https://httpbin.org/headers")
+        
+        request.responseJSON { response in
+            let json = try? response.result.get()
+            print(json.debugDescription)
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 1000
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView
+            .dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? Cell else {
+                return UICollectionViewCell()
+        }
+        
+        cell.backgroundColor = .black
+        
+        cell.imageView.loadImage(from: "https://robohash.org/\(indexPath.row)?size=600x600", placeHolder: UIImage(named:"0"))
+        // Configure the cell
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    }
+}
+
